20580cf4426345534519f390feb22aa2bd43aebc
mgs1012 <maria.mgs2602@gmail.com>
2015-03-05 13:53:29 +0100

    Add Class Test: Tema Add Class Test: Voto PENDIENTES DE IMPLEMENTACION

---
+++ src/test/java/es/miw/upm/persistence/models/daos/jdbc/TemaTest.java
@@ -0,0 +1,14 @@
+package es.miw.upm.persistence.models.daos.jdbc;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TemaTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
