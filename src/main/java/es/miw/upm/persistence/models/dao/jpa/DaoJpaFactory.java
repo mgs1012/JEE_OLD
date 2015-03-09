fd2959d0a4f5a1b8e28253b9c5e9a62afce84b4d
Maria <Maria@Maria-PC>
2015-03-09 21:18:58 +0100

    Update estructura Dao.JPA class DaoJpaFactory class GenericDaoJpa

---
+++ src/main/java/es/miw/upm/persistence/models/dao/jpa/DaoJpaFactory.java
@@ -0,0 +1,35 @@
+package es.miw.upm.persistence.models.dao.jpa;
+
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import org.apache.logging.log4j.LogManager;
+
+import es.miw.upm.persistence.models.daos.DaoFactory;
+import es.miw.upm.persistence.models.daos.TemaDao;
+import es.miw.upm.persistence.models.daos.VotoDao;
+
+public class DaoJpaFactory extends DaoFactory {
+    private static final String PERSISTENCE_UNIT = "Votaciones";
+
+    private static EntityManagerFactory entityManagerFactory = Persistence
+            .createEntityManagerFactory(PERSISTENCE_UNIT);
+
+    public DaoJpaFactory() {
+        LogManager.getLogger(DaoJpaFactory.class).debug("create Entity Manager Factory");
+    }
+
+    public static EntityManagerFactory getEntityManagerFactory() {
+        return entityManagerFactory;
+    }
+
+	@Override
+	public TemaDao getTemaDao() {
+		return new TemaDaoJpa();
+	}
+
+	@Override
+	public VotoDao getVotoDao() {
+		return new VotoDaoJpa();
+	}
+}
