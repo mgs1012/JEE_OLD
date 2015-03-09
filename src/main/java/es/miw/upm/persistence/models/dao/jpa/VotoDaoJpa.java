0f1627b688745e2f953c16051b55d67e7535bb45
Maria <Maria@Maria-PC>
2015-03-09 19:28:08 +0100

    Paquete models.dao.jpa Add class VotoDaoJpa Update class TemaDaoJpa

---
+++ src/main/java/es/miw/upm/persistence/models/dao/jpa/VotoDaoJpa.java
@@ -0,0 +1,70 @@
+package es.miw.upm.persistence.models.dao.jpa;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import es.miw.upm.persistence.jpa.Tema;
+import es.miw.upm.persistence.models.utils.NivelEstudios;
+
+public class VotoDaoJpa extends GenericDaoJpa<Voto, Integer> implements VotoDao{
+	
+	@Id
+    @GeneratedValue
+    private Integer id;
+
+    private String ip;
+
+    private NivelEstudios nivelEstudios;
+
+    @ManyToOne
+    @JoinColumn
+    private Tema tema;
+
+    public Voto(Integer id, String ip, NivelEstudios nivelEstudios, Tema tema) {
+        this.id = id;
+        this.ip = ip;
+        this.nivelEstudios = nivelEstudios;
+        this.tema = tema; 
+        
+    }
+
+    public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getIp() {
+		return ip;
+	}
+
+	public void setIp(String ip) {
+		this.ip = ip;
+	}
+
+	public NivelEstudios getNivelEstudios() {
+		return nivelEstudios;
+	}
+
+	public void setNivelEstudios(NivelEstudios nivelEstudios) {
+		this.nivelEstudios = nivelEstudios;
+	}
+
+	public Tema getTema() {
+		return tema;
+	}
+
+	public void setTema(Tema tema) {
+		this.tema = tema;
+	}
+
+	public Voto() {
+    	
+    }
+
+
+}
