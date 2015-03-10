f68bc7d7c53ddc91c17f61c1305fae70bbba977f
mgs1012 <maria.mgs2602@gmail.com>
2015-03-10 10:24:23 +0100

    Pruebas JSF - estructura

---
+++ src/main/java/es/miw/upm/web/jsf/HelloJSFBean.java
@@ -0,0 +1,13 @@
+package es.miw.upm.web.jsf;
+
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean
+public class HelloJSFBean {
+    private String name="Hola, desde Bean. OK!!!";
+
+    public String getName() {
+        return name;
+    }
+    
+}
