b3fdec1ba58f50b26e0bf903be8fbeede78aef21
mgs1012 <maria.mgs2602@gmail.com>
2015-03-10 15:09:31 +0100

    Refactor DaoJpaFactory

--- src/main/java/es/miw/upm/persistence/models/dao/jpa/DaoJpaFactory.java
+++ src/main/java/es/miw/upm/persistence/models/dao/jpa/DaoJpaFactory.java
@@ -1,10 +1,18 @@
 package es.miw.upm.persistence.models.dao.jpa;
 
+import java.sql.SQLException;
+import java.sql.Statement;
+
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
 import org.apache.logging.log4j.LogManager;
 
+import es.art83.persistence.models.daos.jdbc.CategoryDaoJdbc;
+import es.art83.persistence.models.daos.jdbc.DaoJdbcFactory;
+import es.art83.persistence.models.daos.jdbc.UserDaoJdbc;
+import es.art83.persistence.models.entities.Category;
+import es.art83.persistence.models.entities.User;
 import es.miw.upm.persistence.models.daos.DaoFactory;
 import es.miw.upm.persistence.models.daos.TemaDao;
 import es.miw.upm.persistence.models.daos.VotoDao;
@@ -32,4 +40,5 @@ public class DaoJpaFactory extends DaoFactory {
 	public VotoDao getVotoDao() {
 		return new VotoDaoJpa();
 	}
+
 }
